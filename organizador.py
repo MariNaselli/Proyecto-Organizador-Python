c09799e914ba9b642d3dc512daa266d1a3a61ef3
Mariana Naselli <nasellimariana@outlook.com>
2026-01-13 15:24:53 -0300

    filtro de archivos protegidos

--- organizador.py
+++ organizador.py
@@ -4,7 +4,11 @@ archivos = os.listdir('.') # solicitamos lista de archivos en esta misma carpeta
 print(archivos)
 
 for nombre_completo in archivos:
+    
+    if os.path.isdir(nombre_completo) or nombre_completo in ['organizador.py', 'README.md', '.gitignore']:
+        continue
     nombre, extension = os.path.splitext(nombre_completo)
-    #la herramienta os.path.splitext separa el nombre de la extensión
+    extension = extension.lower()
+    #la herramienta os.path.splitext separa el nombre de la extensión. Busca el último punto de derecha a izquierda.
     print(f"Archivo: {nombre} | Extensión: {extension}")
     
