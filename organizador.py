c295f21e9f961162e83fc09eec3dcfa69370e014
Mariana Naselli <nasellimariana@outlook.com>
2026-01-13 15:05:32 -0300

    separar nombres de extensiones usando splitext

--- organizador.py
+++ organizador.py
@@ -3,3 +3,8 @@ import os
 archivos = os.listdir('.') # solicitamos lista de archivos en esta misma carpeta
 print(archivos)
 
+for nombre_completo in archivos:
+    nombre, extension = os.path.splitext(nombre_completo)
+    #la herramienta os.path.splitext separa el nombre de la extensión
+    print(f"Archivo: {nombre} | Extensión: {extension}")
+    
